1dbcb76b3a7836f3d84a940af80681aaa4a76380
Desmond <desmondmpofu@gmail.com>
2024-03-17 04:03:30 +0200

    v1 input using state variables

--- refs/src/components/Player.jsx
+++ refs/src/components/Player.jsx
@@ -1,10 +1,24 @@
+import React, {useState} from "react";
+
 export default function Player() {
+    const [name, setName] = useState("unknown entity");
+    const [didSubmit, setDidSubmit] = useState(false);
+
+    const handleSetName = (event) => {
+        setDidSubmit(false);
+        setName(event.target.value);
+    }
+
+    const handleSubmit = () => {
+        setDidSubmit(true);
+    }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {didSubmit? name:'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleSetName} />
+        <button onClick={handleSubmit}>Set Name</button>
       </p>
     </section>
   );
